3a40ba6ea2225c7324892a45c62f8adef7f9f8c4
Felipe Espinoza <fespinozacast@gmail.com>
2019-01-18 14:46:57 +0100

    Run `rubocop --safe-auto-correct`

--- lib/pr_changelog/git_proxy.rb
+++ lib/pr_changelog/git_proxy.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 module PrChangelog
   class GitProxy
-    LOG_FORMAT = '- %cn: %s%n%w(80, 2, 2)%b'.freeze
+    LOG_FORMAT = '- %cn: %s%n%w(80, 2, 2)%b'
 
     def merge_commits_between(base_ref, current_ref)
       `git log --merges #{base_ref}..#{current_ref} --format='#{LOG_FORMAT}'`
     end
   end
 end
-
